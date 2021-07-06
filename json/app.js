d19717a963647a3d2554380e36b9e362b98700f4
Taemin <dbsxoals689@naver.com>
2021-07-06 19:34:26 +0900

    서버로 json 파일 로드를 위한 파일 추가

---
+++ json/app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+
+const inPort = 3000;            // 내부 포트 번호
+
+var app = http.createServer(function(request, response) {
+    fs.readFile('data.json', function(error, data) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/json' });
+            response.end(data);
+        }
+    })
+});
+
+app.listen(inPort, function() {       // 내부 포트 번호: 80, 외부 포트 번호는 웹브라우저의 주소창에 입력
+  console.log("HRC node.js is running on port %d", inPort);
+});
