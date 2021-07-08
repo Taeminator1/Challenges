fe461d284100262209ea59766fff9d05380e9b7c
Taemin <dbsxoals689@naver.com>
2021-07-09 04:45:24 +0900

    CollectionView를 이용해 label 1열로 띄움

---
+++ PhotosViewer/PhotosViewer/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  PhotosViewer
+//
+//  Created by 윤태민 on 7/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var list = ["1", "2", "3", "4"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.backgroundColor = .gray
+        cell.label.text = list[indexPath.row]
+        
+        return cell
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+}
+
+// cell layout
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+    // 위 아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    // 옆 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    // cell 사이즈( 옆 라인을 고려하여 설정 )
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let width = collectionView.frame.width
+        print("collectionView width=\(collectionView.frame.width)")
+        print("cell하나당 width=\(width)")
+        print("root view width = \(self.view.frame.width)")
+
+        let size = CGSize(width: width, height: width)
+        return size
+    }
+}
